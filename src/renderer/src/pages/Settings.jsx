9f3b0600836d6081b90579d15a2fed0fc3f80a24
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-27 08:04:04 +0530

    Update Settings.jsx

--- src/renderer/src/pages/Settings.jsx
+++ src/renderer/src/pages/Settings.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className=''>
+        <div className="flex justify-between">
+            <h1 className="p-4 text-xl text-gray-500">Settings</h1>
+        </div>
+        <hr />
+    </div>
   )
 }
 
