e6082600fb061902dadc670d0fd6f2fa7b0636c3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-27 10:07:01 +0530

    Update Collection.jsx

--- src/renderer/src/pages/Collection.jsx
+++ src/renderer/src/pages/Collection.jsx
@@ -37,31 +37,34 @@ const Collection = () => {
   }
 
   return (
-    <div className="p-4">
-      <div className="flex justify-between">
+    <div className="">
+      <div className="flex justify-between p-4">
         <h1 className="text-xl text-gray-500">My Collections</h1>
         <div className="flex cursor-pointer" onClick={CreateCollection}>
           <MdOutlineAddBox className="text-gray-500 h-8 w-auto" />
-          <h1 className="text-gray-500">Create</h1>
+          <h1 className="text-gray-500 pt-1">Create</h1>
         </div>
       </div>
 
       <hr />
 
-      {CreateCollectionClick && (
-        <div className="bg-gray-200/50 shadow rounded border p-4 m-2">
-          <div className="flex justify-between">
-            <h1>Create New Collection</h1>
-            <IoMdCloseCircle className="text-red-500 h-6 w-auto cursor-pointer" onClick={CloseCollection} />
+      {
+        CreateCollectionClick ? 
+          <div className="bg-gray-200/50 shadow rounded border p-4 m-2">
+            <div className="flex justify-between">
+              <h1>Create New Collection</h1>
+              <IoMdCloseCircle className="text-red-500 h-6 w-auto cursor-pointer" onClick={CloseCollection} />
+            </div>
+
+            <CollectionForm onAddCollection={handleAddCollection} />
           </div>
+        :
+          <div className=""></div>
+      }
 
-          <CollectionForm onAddCollection={handleAddCollection} />
-        </div>
-      )}
-
-      <div className="py-4">
-        <h1 className="text-xl">My Collection List</h1>
-        <ul className="space-y-2">
+      <div className="p-4 ">
+        <h1 className="text-xl text-gray-500">My Collection List</h1>
+        <ul className="space-y-2 pt-6 border-b">
           {Object.keys(collections).map((collection, index) => (
             <li key={index} className="p-2 bg-gray-100 rounded">{collection}</li>
           ))}
